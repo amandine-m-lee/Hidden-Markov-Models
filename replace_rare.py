7ff89e14b6948432921baee2799827454c13ad9b
Amandine Lee <amandine.m.lee@gmail.com>
2013-06-26 14:48:43 -0400

    replace_rare.py has efficiency testing, associated lprof file

--- replace_rare.py
+++ replace_rare.py
@@ -1,47 +1,52 @@
-"""This program takes all of the words with counts < 5 and replaces
-thier name with '_RARE_' to estimate the emmission probability of 
-words not seen before"""
+@profile
+def replace_rare():
 
-"""countsrcfile = raw_input("Count source file: ")
-trainsrcfile = raw_input("Training source file: ")
-destfile = raw_input("New training file destination: ")"""
+    """This program takes all of the words with counts < 5 and replaces
+    thier name with '_RARE_' to estimate the emmission probability of 
+    words not seen before"""
 
-countsrcfile = "gene.counts"
-trainsrcfile = "short.train"
-destfile = "new.train"
+    """countsrcfile = raw_input("Count source file: ")
+    trainsrcfile = raw_input("Training source file: ")
+    destfile = raw_input("New training file destination: ")"""
 
-srcfreq = open(countsrcfile)
+    countsrcfile = "gene.counts"
+    trainsrcfile = "short.train"
+    destfile = "new.train"
 
-rarewords = []
-rarecount = 0
+    srcfreq = open(countsrcfile)
 
-for line in srcfreq:
-    parts = line.split(' ')
-    if parts[1] == 'WORDTAG':
-        word = parts[3]
-        freq = int(parts[0])
-    
-        if freq < 5:
-          #  print word
-            rarewords.append(word.strip())
-            rarecount += freq
-    else:
-        break
+    rarewords = []
+    rarecount = 0
 
-srctrain = open(trainsrcfile)
-dest = open(destfile, 'w')
+    for line in srcfreq:
+        parts = line.split(' ')
+        if parts[1] == 'WORDTAG':
+            word = parts[3]
+            freq = int(parts[0])
+        
+            if freq < 5:
+              #  print word
+                rarewords.append(word.strip())
+                rarecount += freq
+        else:
+            break
 
-linnum =0
+    srctrain = open(trainsrcfile)
+    dest = open(destfile, 'w')
 
-for line in srctrain:
-    words = line.split(' ')
-    linnum += 1
-    print linnum
-    if len(words) == 2:
-        if words[0] in rarewords:
-            dest.write('__RARE__ ' + words[1])
+    linnum =0
+
+    for line in srctrain:
+        words = line.split(' ')
+        linnum += 1
+       # print linnum
+
+        if len(words) == 2:
+            if words[0] in rarewords:
+                dest.write('__RARE__ ' + words[1])
+            else:
+                dest.write(line)
         else:
             dest.write(line)
-    else:
-        dest.write(line)
 
+replace_rare()
